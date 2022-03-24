942714350344c7577a13de28848b566b4d995ed2
justlilith <hello@justlilith.com>
2022-03-24 07:06:45 -0500

    clean up main a bit -L6

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ fn main() {
     let store = State::new();
     let mut app = Nickel::new();
     app.utilize(router(store));
-    
     app.listen("127.0.0.1:3000").unwrap();
 }
 
@@ -21,45 +20,15 @@ fn router(store: State) -> nickel::Router {
     let mut router = nickel::Router::new();
     router.get("/", middleware! {|req| hello(req)});
     let store2 = store.clone();
-    router.get(
-        "/get/:key",
-        middleware! {|req| {
-            store2.get(req)
-        }},
-    );
+    router.get("/get/:key", middleware! {|req| store2.get(req)});
     let store3 = store.clone();
-    router.get(
-        "/add/:key/:value",
-        middleware! {|req| {
-            store3.add(req)
-        }},
-    );
+    router.get("/add/:key/:value", middleware! {|req| store3.add(req)});
     let store4 = store.clone();
-    router.get(
-        "/delete/:key",
-        middleware! {|req| {
-            store4.delete(req)
-        }},
-    );
+    router.get("/delete/:key", middleware! {|req| store4.delete(req)});
     let store5 = store.clone();
-    router.get(
-        "/purge",
-        middleware! {|_| {
-            store5.purge()
-        }},
-    );
+    router.get("/purge", middleware! {|_| store5.purge()});
     let store6 = store.clone();
-    router.get(
-        "/contents",
-        middleware! {|_| {
-            store6.contents()
-        }},
-    );
-    router.get(
-        "/update/:key/:value",
-        middleware! {|req| {
-            store.add(req)
-        }},
-    );
+    router.get("/contents", middleware! {|_| store6.contents()});
+    router.get("/update/:key/:value", middleware! {|req|store.add(req)});
     router
 }
